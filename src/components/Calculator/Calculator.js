360230d4cc2ec5471662b53a313a85264b33bf0e
Marina Malyarchuk <pchupchu@yandex.ru>
2023-10-10 21:14:25 +0300

    Add Calculator Form desktop

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -7,60 +7,70 @@ function Calculator() {
         <form className="calculator__form">
           <h2 className="calculator__title">Enter your details below</h2>
           <fieldset className="calculator__system">
-            <label for="metric" className="calculator__system-label">
-              Metric
-            </label>
-            <input
-              type="radio"
-              name="choice"
-              id="metric"
-              value="metric"
-              checked
-              className="calculator__choice"
-            />
-            <label for="imperial" className="calculator__system-label">
-              Imperial
-            </label>
-            <input
-              type="radio"
-              name="choice"
-              id="imperial"
-              value="imperial"
-              className="calculator__choice"
-            />
+            <div class="calculator__custom-radio">
+              <input
+                type="radio"
+                name="choice"
+                id="metric"
+                value="metric"
+                className="calculator__choice"
+                checked
+              />
+              <label for="metric" className="calculator__system-label">
+                Metric
+              </label>
+            </div>
+            <div class="calculator__custom-radio">
+              <input
+                type="radio"
+                name="choice"
+                id="imperial"
+                value="imperial"
+                className="calculator__choice"
+              />
+              <label for="imperial" className="calculator__system-label">
+                Imperial
+              </label>
+            </div>
           </fieldset>
           <fieldset class="calculator__info">
-            <label for="height" className="calculator__label">
-              Height
-            </label>
-            <input
-              type="text"
-              className="calculator__input"
-              id="height"
-              name="height"
-              placeholder=""
-              required
-              // value={values.height || ""}
-              // onChange={handleChange}
-            />
-            <label for="weight" className="calculator__label">
-              Weight
-            </label>
-            <input
-              type="text"
-              className="calculator__input"
-              id="weight"
-              name="weight"
-              placeholder=""
-              required
-              // value={values.height || ""}
-              // onChange={handleChange}
-            />
+            <div className="calculator__info-container">
+              <label for="height" className="calculator__label">
+                Height
+              </label>
+              <input
+                type="number"
+                className="calculator__input"
+                id="height"
+                name="height"
+                placeholder="0"
+                required
+                // value={values.height || ""}
+                // onChange={handleChange}
+              />
+            </div>
+            <div className="calculator__info-container">
+              <label for="weight" className="calculator__label">
+                Weight
+              </label>
+              <input
+                type="number"
+                className="calculator__input"
+                id="weight"
+                name="weight"
+                placeholder="0"
+                required
+                // value={values.height || ""}
+                // onChange={handleChange}
+              />
+            </div>
           </fieldset>
         </form>
         <div className="calculator__result">
-          <p className="calculator__result-desc">Your BMI is...</p>
-          <h3 className="calculator__weight">23.4</h3>
+          <div className="calculator__inner">
+            <p className="calculator__result-desc">Your BMI is...</p>
+            <h3 className="calculator__weight">23.4</h3>
+          </div>
           <p className="calculator__text">
             Your BMI suggests you’re{" "}
             <span className="calculator__text calculator__classification">
